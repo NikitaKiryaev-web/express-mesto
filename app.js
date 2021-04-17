0f8727dbf5c0e324202ebbccc4397510229f88ca
Nikita Kiryaev <kiryaev.work@yandex.ru>
2021-04-17 18:05:21 +0300

    fix: разрешить CORS для фронта

--- app.js
+++ app.js
@@ -6,9 +6,15 @@ const userRouter = require('./routes/users');
 const cardRouter = require('./routes/cards');
 const { createUser, login } = require('./controllers/users');
 const auth = require('./middlewares/auth');
+const {requestLogger, errorLogger} = require('./middlewares/logger');
+const cors = require('cors');
 
 dotenv.config();
 const { PORT = 3000 } = process.env;
+
+const corsOptions = {
+  origin: 'http://kiryaev.students.nomoredomains.club'
+};
 
 const app = express();
 
@@ -18,8 +24,12 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
   useFindAndModify: false,
 });
 
+app.use(cors(corsOptions));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(requestLogger);
 
 app.post('/signin', login);
 app.post('/signup', createUser);
@@ -28,6 +38,8 @@ app.use(auth);
 
 app.use('/', userRouter);
 app.use('/', cardRouter);
+
+app.use(errorLogger);
 
 app.use((err, req, res, next) => {
   const { statusCode = 500, message } = err;
